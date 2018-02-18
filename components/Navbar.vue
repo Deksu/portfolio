de9d5d878c12fc99773291d59eca229a4ae6564f
Deksu <minxzu@gmail.com>
2018-02-19 01:46:28 +0200

    fixing mobile nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,11 +3,16 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="/">Portfolio</a>
-        <button class="button navbar-burger">
-          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
-          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
-          <span></span>
-        </button>
+
+            <!-- Using a <label /> here -->
+    <label class="nav-toggle" for="nav-toggle-state">
+    <span>test</span>
+    <span></span>
+    <span></span>
+  </label>
+
+  <!-- This checkbox is hidden -->
+  <input type="checkbox" id="nav-toggle-state" />
       </div>
       <div class="navbar-menu">
         <div class="navbar-end">
@@ -22,6 +27,14 @@
 </template>
 
 <style scoped>
+
+#nav-toggle-state {
+  display: none;
+}
+
+#nav-toggle-state:checked ~ .nav-menu {
+  display: block;
+}
 
 /* a.navbar-item:hover, a.navbar-item.is-active,
 a.navbar-link:hover,
