03637b810e8e43bc4cdfc11d18294fcc5d9cee28
Deksu <minxzu@gmail.com>
2018-02-20 17:12:45 +0200

    added static mobile navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,24 +2,16 @@
   <div class="container">
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="/">Portfolio</a>
+        <a class="navbar-item hide-on-mobile" href="/">Portfolio</a>
 
-          <div v-show="isShow"  class="content">
-            <div class="navbar-brand flow">
+          <div class="content">
+            <div class="navbar-brand">
             <a class ="navbar-item" href="/">Home</a>
             <a class ="navbar-item" href="/about">About</a>
             <a class ="navbar-item" href="/projects">Projects</a>
             <a class ="navbar-item" href="/contact">Contact</a>
             </div>
           </div>
-
-
-          <button  v-on:click="isShow = !isShow" class="button navbar-burger control">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-
       </div>
 
 
@@ -38,13 +30,7 @@
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-    isShow: false
-    };
-  }
-}
+
 </script>
 
 
@@ -54,7 +40,7 @@ export default {
 .content {
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: center;
   flex-shrink: 1;
   width: 100%;
   margin-left: 10px;
@@ -66,6 +52,13 @@ export default {
     display: none;
   }
 }
+
+@media screen and (max-width: 1023px) {
+  .hide-on-mobile {
+    display: none;
+  }
+}
+
 /* a.navbar-item:hover, a.navbar-item.is-active,
 a.navbar-link:hover,
 a.navbar-link.is-active {
