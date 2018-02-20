e5d4f047193385d82e96e32882e0b3efc6d60e06
Deksu <minxzu@gmail.com>
2018-02-20 18:27:30 +0200

    fix project spacing

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <h1 class="title">Hello, my name is Miikka Marin</h1>
       <h2 class="subtitle">web design enthusiast from Finland</h2>
 
-      <p id="welcome-text">Welcome to my new (but not finished!) portfolio site. Hope you enjoy my <a href="/projects">work.</a></p>
+      <p id="welcome-text">Welcome to my new (but not quite finished!) portfolio site. Hope you enjoy my <a href="/projects">work.</a></p>
     </div>
   </section>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -9,11 +9,11 @@
             <h1 class="title-1">Pizza Online-concept</h1>
             <h2 class="subtitle-2">School project</h2>
     <div class="snapshot-container">
-        <img src="~/static/pizzaonline/frontpage.png" alt="pizzaonline-frontpage" class="snapshot-3-img" id="image1">
+        <img src="~/static/pizzaonline/frontpage.png" alt="pizzaonline-frontpage" class="snapshot-3 snapshot-img" id="image1">
 
-        <img src="~/static/pizzaonline/signup.png" alt="pizzaonline-signup" class="snapshot-3-img" id="image2">
+        <img src="~/static/pizzaonline/signup.png" alt="pizzaonline-signup" class="snapshot-3 snapshot-img" id="image2">
 
-        <img src="~/static/pizzaonline/shopping-cart.png" alt="pizzaonline-shoppingcart" class="snapshot-3-img" id="image3">
+        <img src="~/static/pizzaonline/shopping-cart.png" alt="pizzaonline-shoppingcart" class="snapshot-3 snapshot-img" id="image3">
     </div>
 
 
@@ -24,6 +24,7 @@
             </div>
         <img src="~/static/fsharp/f-sharp-frontpage.png" alt="fsharp-frontpage" class="snapshot-img" id="image1">
         </div>
+
                 <div class="snapshot-container-2 last-snapshot-container">
             <div>
             <h1 class="title-2">Goldsmiths' portfolio website</h1>
@@ -31,7 +32,7 @@
             </div>
         <img src="~/static/mm/mm-frontpage.png" alt="mm-frontpage" class="snapshot-img" id="image1">
 
-    </div>
+                </div>
   </section>
 </template>
 
@@ -45,31 +46,26 @@
     align-items: flex-start;
     margin: auto;
 }
-/* .snapshot-container img {
-    width: 30%;
-    height: 30%;
-    margin-left: 10px;
-    margin-right: 10px;
-    border: 8px solid #000;
-} */
 
-.snapshot-3-img {
+.snapshot-3 {
     width: 40%;
     height: 60%;
     margin: 20px 20px 20px 10px;
     border: 3px solid #000;
+}
+
+.snapshot-img:hover {
     transition: -webkit-transform 0.25s ease;
     transition: transform 0.25s ease;
-}
-
-.snapshot-3-img:hover {
     -webkit-transform: scale(1.2);
     transform: scale(1.2);
 }
 
-.snapshot-3-img:focus {
-    -webkit-transform: scale(1.2);
-    transform: scale(1.2);
+.snapshot-img:focus {
+    transition: -webkit-transform 0.25s ease;
+    transition: transform 0.25s ease;
+    -webkit-transform: scale(1.6);
+    transform: scale(1.6);
 }
 
 .snapshot-container-2 {
@@ -78,12 +74,15 @@
     flex-direction: row;
     justify-content: space-around;
     align-items: center;
+    align-content: center;
     margin: 200px 0px 200px 0px;
+    background-color: #ccc;
 }
 
 .snapshot-container-2 img {
+    
     width: 40%;
-    height: 40%;
+    height: 60%;
     margin: 30px 10px 30px 10px;
 }
 
@@ -102,7 +101,7 @@
     font-family: "Work Sans";
     font-size: 1.5em;
     font-weight: 600;
-    margin: 50px 0px 50px 0px;
+    margin: auto;
     text-align: center;
 }
 
